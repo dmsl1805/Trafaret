399a58ffeeee2ffbfe86636794f5ffcb5df17937
Dmitriy Shulzhenko <dmsl1805@gmail.com>
2023-08-08 13:52:08 +0300

    fix availability

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Trafaret",
     platforms: [
-      .iOS(.v16),
+      .iOS(.v13),
       .macOS(.v10_15),
       .tvOS(.v13),
       .watchOS(.v6),
